9a8c192448429cdd002a1d7cb5730f62800a32f3
Felipe Maruyama <felipe-eiji@hotmail.com>
2021-10-09 19:17:13 -0300

    refactor: transform SignUpController handle method to async

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -1,5 +1,5 @@
-import { HttpRequest } from './http'
+import { HttpRequest, HttpResponse } from '.'
 
 export interface Controller {
-  handle: (httpRequest: HttpRequest) => any
+  handle: (httpRequest: HttpRequest) => Promise<HttpResponse>
 }
